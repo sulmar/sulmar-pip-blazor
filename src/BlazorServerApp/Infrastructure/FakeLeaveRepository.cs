08cabdb89a9542c682bfd950b3d0c0be47681b54
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-07-10 13:44:26 +0200

    Dodanie statusu

--- src/BlazorServerApp/Infrastructure/FakeLeaveRepository.cs
+++ src/BlazorServerApp/Infrastructure/FakeLeaveRepository.cs
@@ -11,11 +11,11 @@ public class FakeLeaveRepository : ILeaveRepository
     {
         leaves = new List<Leave>
         {
-            new Leave { Employee = new User { Id = 1, FirstName = "Kate", LastName = "Smith"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Paid},
-            new Leave { Employee = new User { Id = 2, FirstName = "Bob", LastName = "Smith"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Paid},
-            new Leave { Employee = new User { Id = 3, FirstName = "John", LastName = "Smith"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Unpaid},
-            new Leave { Employee = new User { Id = 4, FirstName = "Ann", LastName = "Spider"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Maternity},
-            new Leave { Employee = new User { Id = 5, FirstName = "John", LastName = "Spider"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Paid},
+            new Leave { Employee = new User { Id = 1, FirstName = "Kate", LastName = "Smith"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Paid, Status = LeaveStatus.Awaiting },
+            new Leave { Employee = new User { Id = 2, FirstName = "Bob", LastName = "Smith"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Paid, Status = LeaveStatus.Awaiting },
+            new Leave { Employee = new User { Id = 3, FirstName = "John", LastName = "Smith"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Unpaid, Status = LeaveStatus.Accepted },
+            new Leave { Employee = new User { Id = 4, FirstName = "Ann", LastName = "Spider"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Maternity, Status = LeaveStatus.Cancelled},
+            new Leave { Employee = new User { Id = 5, FirstName = "John", LastName = "Spider"}, From = DateTime.Parse("2024-07-10"), To =  DateTime.Parse("2024-07-17"), LeaveType = LeaveType.Paid, Status = LeaveStatus.Accepted},
 
         };
     }
--- src/BlazorServerApp/Models/Leave.cs
+++ src/BlazorServerApp/Models/Leave.cs
@@ -6,4 +6,12 @@ public class Leave : BaseEntity
     public LeaveType LeaveType { get; set; }
     public DateTime From { get; set; }
     public DateTime To { get; set; }
+    public LeaveStatus Status { get; set; }
+}
+
+public enum LeaveStatus
+{
+    Awaiting,
+    Accepted,
+    Cancelled
 }
