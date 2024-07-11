6189199d26b1d03794f6c767a03649a4d1071cb1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-07-11 08:49:26 +0200

    Dodanie akceptacji i anulowania urlopu

--- src/BlazorServerApp/Models/Leave.cs
+++ src/BlazorServerApp/Models/Leave.cs
@@ -7,6 +7,26 @@ public class Leave : BaseEntity
     public DateTime From { get; set; }
     public DateTime To { get; set; }
     public LeaveStatus Status { get; set; }
+
+    public void Cancel()
+    {
+        Status = LeaveStatus.Cancelled;
+    }
+
+    public bool CanCancel()
+    {
+        return Status == LeaveStatus.Awaiting || Status == LeaveStatus.Accepted;
+    }
+
+    public void Accept()
+    {
+        Status = LeaveStatus.Accepted;
+    }
+
+    public bool CanAccept()
+    {
+        return Status == LeaveStatus.Awaiting || Status == LeaveStatus.Cancelled;
+    }
 }
 
 public enum LeaveStatus
